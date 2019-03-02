fa626f35427531e087da7346bd141f3e8d720c3c
TangXuanzhao <xuanzhaotang@gmail.com>
2019-03-03 01:24:41 +0800

    add ws

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ composer network install --archiveFile ./dist/block_chain-interface_4.bna --card
 composer network start --networkName block_chain-interface_4 --networkVersion $NETWORK_VERSION --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --file ./dist/networkAdmin.card -o npmrcFile=./npmrc
 composer card delete --card admin@block_chain-interface_4
 composer card import --file ./dist/networkAdmin.card
-# composer-rest-server -c admin@block_chain-interface_4 -n never -p 3000
+# composer-rest-server -c admin@block_chain-interface_4 -n never -p 3000 -w
